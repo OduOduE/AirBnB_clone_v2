6076fdcb0284f94e9e8b7aab48c6798a809d82bc
OduOduE <theoduemmanuel@gmail.com>
2023-04-22 16:40:53 +0000

    h

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """Starts a Flask web application... """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """ Print 'Hello HBNB!' on web """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c(text):
     """Displays 'C' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
@@ -27,8 +27,8 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python")
+@app.route("/python/<text>")
 def python(text="is cool"):
     """Displays 'Python' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
@@ -36,13 +36,13 @@ def python(text="is cool"):
     return "Python {}".format(text.replace("_", " ")
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number/<int:n>")
 def number(n):
     """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>")
 def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", n=n)
