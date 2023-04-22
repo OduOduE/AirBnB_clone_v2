2561ed8a644582ee7cbab5cf6ca2bdb86af03dfa
OduOduE <theoduemmanuel@gmail.com>
2023-04-22 16:46:23 +0000

    h

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -18,36 +18,36 @@ def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>")
+@app.route('/c/<text>')
 def c(text):
     """Displays 'C' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route("/python")
-@app.route("/python/<text>")
-def python(text="is cool"):
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
     """Displays 'Python' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
-    return "Python {}".format(text.replace("_", " ")
+    return 'Python {}'.format(text.replace('_', ' ')
 
 
-@app.route("/number/<int:n>")
+@app.route('/number/<int:n>')
 def number(n):
     """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>")
+@app.route('/number_template/<int:n>')
 def number_template(n):
     """Displays an HTML page only if <n> is an integer."""
     return render_template("5-number.html", number=n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
     # app.run(host="0.0.0.0", port=5000)
